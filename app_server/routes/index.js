634635b8a2f2ff2ad30616b942ff301548a6a3c8
21eg107a28 <@21eg107a28@anurag.edu.in>
2023-08-23 11:19:24 +0530

    second commit

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -4,9 +4,9 @@ var ctrlsongs = require('../controller/songs');
 var ctrlOthers = require('../controller/others');
 /* Locations page */
 router.get('/',ctrlsongs.homelist);
-router.get('/song1',ctrlsongs.songInfo1);
-router.get('/song2',ctrlsongs.songInfo2);
-router.get('/song3',ctrlsongs.songInfo3);
+router.get('/song1',ctrlsongs.SongInfo1);
+router.get('/song2',ctrlsongs.SongInfo2);
+router.get('/song3',ctrlsongs.SongInfo3);
 router.get('/song/review/new',ctrlsongs.addReview);
 
 /*Others pages*/
