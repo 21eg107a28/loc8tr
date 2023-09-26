02f3b5a1cbbc8db11b3448d887c4b63a88260b72
21eg107a28 <@21eg107a28@anurag.edu.in>
2023-09-26 19:09:18 +0530

    mongodb commit

--- app_api/model/db.js
+++ app_api/model/db.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose'); 
 
 const dbURI = "mongodb://0.0.0.0:27017/loc8tr"; 
+if (process.env.NODE_ENV=='production'){
+     dbURI=process.env.MONGODB_URI;
+}
 mongoose.connect(dbURI); 
 
 mongoose.connection.on('connected',  () => {
